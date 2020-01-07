1d5ddda51590fdeb4f1e2ad0480dac3b8f3d489f
femi3d2y <ifeolu.fayanju@academytrainee.com>
2020-01-07 22:16:01 +0000

    serv4 pulls nothig from database

--- serv/app.py
+++ serv/app.py
@@ -14,7 +14,8 @@ def home():
     form = SubmitForm()
 
     hundred = ''
-    ten =''
+    ten = ''
+    effects = ''
 
     if form.is_submitted():
         response = requests.post('http://flask-service:5001/service')
@@ -23,12 +24,12 @@ def home():
         response1 = requests.post('http://flask-service2:5002/service2')
         ten = response1.json()['value']
 
-      # response2 = requests.get('35.246.77.128:5003')
-      # data2 = response2.json()
+        response2 = requests.post('http://flask-data:5003/data')
+        effects = response2.json()['result']
 
 
 
-    return render_template('home.html', form=form, hundred=hundred, ten=ten)
+    return render_template('home.html', form=form, hundred=hundred, ten=ten, effects=effects)
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
--- serv4/application/routes.py
+++ serv4/application/routes.py
@@ -1,21 +1,22 @@
 from flask import request
 from application import app, db
 from application.models import Effects
-
+import requests
 
 @app.route('/data', methods=['POST'])
-def service4():
-    response = requests.get('35.246.77.128:5001')
-    data = response.json()
+def data():
 
-    response1 = requests.get('35.246.77.128:5002')
-    data1 = response1.json()
+        response = requests.post('http://flask-service:5001/service')
+        hundred = response.json()['value']
 
-    value = data + data1
+        response1 = requests.post('http://flask-service2:5002/service2')
+        ten = response1.json()['value']
 
-    if value == 0:
-        value = 100
+        roll = ten + hundred
+
+        if roll == 0:
+            roll = 100
     
-    effect = Effects.query.filter_by(id = value)
+        effect = Effects.query.filter_by(id = roll).all()
 
-    return effect
+        return {'result':f'{effect}'}
